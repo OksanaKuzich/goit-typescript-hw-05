75c6d3b4b3df8e0ad95ca36ab0b0e242ad80ec1a
OksanaKuzich <komoks255@gmail.com>
2023-01-08 21:37:48 +0200

    use pick (parth of object)

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-// Есть функция которая возвращает Promise, он возвращает массив строк и чисел, опишите правильно тип.
+// #1 Есть функция которая возвращает Promise, он возвращает массив строк и чисел, опишите правильно тип.
 function getPromise(): Promise<Array<string | number>> {
   return new Promise((resolve) => {
     resolve(["Text", 50]);
@@ -8,3 +8,23 @@ function getPromise(): Promise<Array<string | number>> {
 getPromise().then((data) => {
   console.log(data);
 });
+
+// #2 Есть функция, она принимает два аргумента, в один попадает name и color в другую часть position и weight. Нужно явно указать, что эти поля из AllType. И сама функция возвращает AllType. Воспользуйтесь Pick.
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
+
+function compare(
+  top: Pick<AllType, "name" | "color">,
+  bottom: Pick<AllType, "position" | "weight">
+): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
